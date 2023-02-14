9327247e911e63566c7c462bcdfe0f197414d125
Finn Gruwier Larsen <figl@ssi.dk>
2023-02-14 12:01:35 +0100

    Refactor tests.

--- tests/a_single/as_library.py
+++ tests/a_single/as_library.py
@@ -1,5 +1,8 @@
-from partitioning_HC import HC
 from datetime import datetime
+import sys
+sys.path.append('/scripts/')
+
+from partitioning_HC import HC
 
 print(datetime.now())
 hc = HC('test_a')
--- tests/a_single/as_library.sh
+++ tests/a_single/as_library.sh
@@ -1,4 +1,4 @@
-python /scripts/use_hc.py
+python /tests/a_single/as_library.py
 diff /tests/a_single/reference/test_a_clusterComposition.tsv test_a_clusterComposition.tsv
 diff /tests/a_single/reference/test_a_dist.tsv test_a_dist.tsv
 diff /tests/a_single/reference/test_a_partitions.tsv test_a_partitions.tsv
